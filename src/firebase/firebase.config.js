813762e5d8421561ed02420b1a5a92e079799308
UAShoshi <ummeaymonafifha@gmail.com>
2024-05-03 00:32:51 +0600

    firebase rename

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyABvqGB_mPFLzNwG8nsYAB3c3b0oqrDm6E",
-  authDomain: "country-tour-304c1.firebaseapp.com",
-  projectId: "country-tour-304c1",
-  storageBucket: "country-tour-304c1.appspot.com",
-  messagingSenderId: "435909795861",
-  appId: "1:435909795861:web:cfe83fd63fa8e5c352ee1c"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
